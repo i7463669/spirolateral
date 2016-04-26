809aa596932eadcc1a58ab23406fccbf3397aab3
Joe <Joe@localhost.localdomain>
2016-04-26 01:03:02 +0100

    removed some functions. trying to reduce the code and put more of the actions inside the main function. tried to included SDL2 however i cannot find away of including the linking for it. however looking from the results it looks like it could be working but have to get SDL2 working to make sure

--- spirolateral.c
+++ spirolateral.c
@@ -1,5 +1,8 @@
+
 #include <stdio.h>
 #include <math.h>
+//#include <SDL2/SDL.h>
+
 
 #define PI 3.14159265
 
@@ -15,48 +18,84 @@ typedef struct parameters
 
 typedef struct maxCoord
 {
-	int maxX;
-	int maxY;
-	int minX;
-	int minY;
+	float maxX;
+	float maxY;
+	float minX;
+	float minY;
 }maxCoord;
 
-parameters scale_position_check(struct parameters para);
 int open_close_pattern( int, int);
-void overlap_caluclate(struct parameters para, struct maxCoord maxCoord, float *, float *, float *);
+maxCoord line_calculate(struct parameters para, struct maxCoord maxCoord, int *, float *, float *);
 maxCoord boundary_test(float *, float *, struct maxCoord maxCoord);
-
 
 int main (void)
 {
 	parameters para;
 	para.length = 3;
 	para.numOfSeg = 5;
-	para.angle = 36;
-	para.X = 20;
-	para.Y = 0;
-	scale_position_check(para);
-	
-	return 0;
-}
+	para.angle = 40;
 
-parameters scale_position_check(struct parameters para)
-{
-	int opencloseResult;
-	printf("the X andd Y Coordinates, %d:%d\n",para.X, para.Y);
+	para.X = 0;
+	para.Y = 0;
 	
-	opencloseResult = open_close_pattern(para.angle, para.numOfSeg);
-	if (opencloseResult == 0)
+	maxCoord maxCoord;
+	float Xn = para.X;
+	float Yn = para.Y;
+	
+	float *pXn = &Xn;
+	float *pYn = &Yn;
+	int angleN = para.angle;
+	int *pAngleN = &angleN;
+	
+	maxCoord.maxX = *pXn;
+	maxCoord.minX = *pXn;
+	maxCoord.maxY = *pYn;
+	maxCoord.minY = *pYn;
+	
+	/*
+	int winPosX = 100;
+	int winPosY = 100;
+	int winWidth = 200;
+	int winHeight = 200;
+	if( SDL_Init( SDL_INIT_EVERYTHING ) != 0 )
 	{
-		printf("the pattern is closed\n");
+		// Something went very wrong in the initialisation, all we can do is exit 
+		perror("Whoops! Something went very wrong, cannot initialise SDL :(");
+		return -1;
+	}
+
+	// Now we have got SDL initialised, we are ready to create a window!
+	
+	SDL_Window *window = SDL_CreateWindow("My Pointy Window!!!",  // The first parameter is the window title //
+		winPosX, winPosY,
+		winWidth, winHeight,
+		SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+	
+	*/
+	
+	int openCloseResult = open_close_pattern(para.angle, para.numOfSeg);
+	if (openCloseResult == 0)
+	{
+		printf("the pattern is close\n");
+		for(int i = 1; i<=para.numOfSeg; i++)
+		{
+			maxCoord = line_calculate(para, maxCoord, pAngleN, pXn, pYn); //mac coordinates for closed pattern
+			printf("this is the max X: \t %06f\n", maxCoord.maxX);
+			printf("this is the max Y: \t %06f\n", maxCoord.maxY);
+			printf("this is the min X: \t %06f\n", maxCoord.minX);
+			printf("this is the min Y: \t %06f\n", maxCoord.minY);
+		}
 	}
 	else
 	{
 		printf("the pattern is open\n");
+	
 	}
-	return para;
+	
+	
+	
+	return 0;
 }
-
 
 
 int open_close_pattern(int angle, int numOfSeg)
@@ -98,10 +137,22 @@ maxCoord boundary_test(float *pXn, float *pYn, struct maxCoord maxCoord)
 	{
 		maxCoord.maxX = *pXn;
 	}
+	if (*pXn<maxCoord.minX)
+	{
+		maxCoord.minX = *pXn;
+	}
+		if (*pYn>maxCoord.maxY)
+	{
+		maxCoord.maxY = *pYn;
+	}
+	if (*pYn<maxCoord.minY)
+	{
+		maxCoord.minY = *pYn;
+	}
 	return maxCoord;
 }
 
-void overlap_calculate(struct parameters para, struct maxCoord maxCoord, float *pAngleN, float *pXn, float *pYn)
+maxCoord line_calculate(struct parameters para, struct maxCoord maxCoord, int *pAngleN, float *pXn, float *pYn)
 {
 	
 	float Xdif, Ydif;
@@ -111,9 +162,9 @@ void overlap_calculate(struct parameters para, struct maxCoord maxCoord, float *
 	for (int i = 1; i<=para.numOfSeg; i++)
 	{
 		Xdif = para.length*(sin(rad*(*pAngleN)));
-		*pXn +=Xdif;
+		*pXn += Xdif;
 		Ydif = para.length*(cos(rad*(*pAngleN)));
-		*pYn +=Ydif;
+		*pYn += Ydif;
 		
 		maxCoord = boundary_test(pXn, pYn, maxCoord);
 		
@@ -124,45 +175,6 @@ void overlap_calculate(struct parameters para, struct maxCoord maxCoord, float *
 			*pAngleN -=360;
 		}
 	}
-}
-
-maxCoord max_coordinates(struct parameters para, struct maxCoord maxCoord, int opencloseResult)
-{
-
-	
-	float *pXn = NULL;
-	float *pYn = NULL;
-	float *pAngleN = NULL;
-	float angleN;
-	float Xn = para.X;
-	float Yn = para.Y;
-	angleN = para.angle;
-	pAngleN = &(angleN);
-	pXn = &Xn;
-	pYn = &Yn;
-	maxCoord.maxX = *pXn;
-	maxCoord.minX = *pXn;
-	maxCoord.maxY = *pYn;
-	maxCoord.minY = *pYn;
-	
-	if (opencloseResult == 0)
-	{
-		overlap_calculate(para, maxCoord, pAngleN, pXn, pYn);
-		/*
-		do
-		{
-			
-		}while ((Xn && Yn) != (para.X && para.Y));
-		*/
-		
-	}
-	else
-	{
-		for (int i=1; i <= 5; i++)
-		{
-			
-		}
-	}
-	
 	return maxCoord;
 }
+
